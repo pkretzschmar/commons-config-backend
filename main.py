6798b0b6ce050ef43602293b8eb2f082d5efa7cb
Vitor Marthendal <marthendalnunes@gmail.com>
2021-08-12 10:24:26 -0300

    add token-lockup route

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+
+from models.token_lockup import TokenLockupModel
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,8 +16,26 @@ class status(Resource):
             return {'data': error}
 
 
-api.add_resource(status, '/')
+class TokenLockup(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('opening-price', type=float)
+        parser.add_argument('token-freeze', type=float)
+        parser.add_argument('token-thaw', type=float)
+        parameters = parser.parse_args()
+        opening_price = parameters['opening-price']
+        token_freeze_period = parameters['token-freeze']
+        token_thaw_period = parameters['token-thaw']
 
+        token_lockup_model = TokenLockupModel(opening_price=opening_price,
+                                              token_freeze_period=token_freeze_period,
+                                              token_thaw_period=token_thaw_period)
+
+        return token_lockup_model.get_data()
+
+
+api.add_resource(status, '/')
+api.add_resource(TokenLockup, '/token-lockup')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models/token_lockup.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+class TokenLockupModel:
+    def __init__(self,
+                 opening_price=None,
+                 token_freeze_period=None,
+                 token_thaw_period=None):
+        self.opening_price = opening_price if opening_price is not None else 100
+        self.token_freeze_period = token_freeze_period if token_freeze_period is not None else 10
+        self.token_thaw_period = token_thaw_period if token_thaw_period is not None else 30
+
+    def get_data(self):
+        weekly_token_thaw = self.opening_price / self.token_thaw_period
+        df = pd.DataFrame({'week': range(1, 61)})
+        df['price'] = 0
+        df.loc[df['week'] <= self.token_freeze_period, 'price'] = self.opening_price
+        df.loc[df['week'] > self.token_freeze_period, 'price'] = (self.opening_price - (df['week'] - self.token_freeze_period) * weekly_token_thaw)
+        df.loc[df['price'] < 0, 'price'] = 0
+        return df.to_dict(orient='list')
