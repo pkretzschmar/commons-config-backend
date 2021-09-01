0e42b2c251e8a7f43ceb276220dec4be17192aae
Vitor Marthendal <marthendalnunes@gmail.com>
2021-09-01 18:20:09 -0300

    add conviction growth to conviction voting model

---
+++ models/conviction_voting.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+class ConvictionVotingModel:
+    def __init__(self,
+                 spending_limit=None,
+                 minimum_conviction=None,
+                 conviction_growth=None):
+        self.spending_limit = spending_limit if spending_limit is not None else 0.2
+        self.minimum_conviction = minimum_conviction if minimum_conviction is not None else 0.05
+        self.conviction_growth = conviction_growth if conviction_growth is not None else 2
+        self.staked_on_proposal = 1
+        self.staked_on_other_proposals = 0
+        self.min_active_stake_pct = 0.05
+        self.output_dict = {}
+        self.output_dict['input'] = {
+            'spendingLimit': self.spending_limit,
+            'minimumConviction': self.minimum_conviction,
+            'convictionGrowth': self.conviction_growth,
+        }
+    
+    def get_decay(self):
+        return (1 / 2) ** (1 / self.conviction_growth)
+    
+    def get_conviction(self, initial_conviction, amount, time):
+        return initial_conviction * self.get_decay() ** time + (amount * (1 - self.get_decay() ** time)) / (1 - self.get_decay())
+
+    def get_max_conviction(self, amount):
+        return amount / (1 - self.get_decay())
+    
+    def get_staked_on_proposals(self):
+        return self.staked_on_proposal + self.staked_on_other_proposals
+
+    def get_staked(self, staked = None):
+        if staked is None:
+            staked = self.get_staked_on_proposals()
+        return np.where(staked > self.min_active_stake_pct, staked, self.min_active_stake_pct)
+
+
+    def get_data(self):
+        # Conviction Growth Chart Data
+        x = np.linspace(0, 5 * self.conviction_growth,100)
+        y = 100 * self.get_conviction(0, self.staked_on_proposal, time=x) / self.get_max_conviction(self.get_staked())
+        df_growth = pd.DataFrame(zip(x, y), columns=['time (days)','conviction (%)'])
+        
+        x2 = np.linspace(5 * self.conviction_growth,10 * self.conviction_growth,1000)
+        y2 = y[-1] * self.get_decay()**x
+        df_decay = pd.DataFrame(zip(x2, y2), columns=['time (days)','conviction (%)'])
+        
+        df = pd.concat([df_growth, df_decay])
+        self.output_dict['output'] = {'convictionGrowthChart' : df.to_dict(orient='list')}
+
+        return self.output_dict
