601c3b52195c3b8e5a5f4ba51980c2b7a05d87d3
PabloCGL <50929773+PabloCGL@users.noreply.github.com>
2021-09-16 19:16:17 +0200

    small output changes

--- models/augmented_bonding_curve.py
+++ models/augmented_bonding_curve.py
@@ -39,7 +39,7 @@ class BondingCurveInitializer:
         df = self.curve_over_balance(supply_ref-10, supply_ref, 100000)
         df_rounded = df.round(3)
         
-        index= int(df.index.where(df_rounded['Balance (in thousands)'] >= balance).dropna()[0])
+        index= int(df.index.where(df_rounded['balanceInThousands'] >= balance).dropna()[0])
         price = df.at[index, "price"]
 
         supply = balance/(price* self.reserve_ratio())
@@ -48,18 +48,18 @@ class BondingCurveInitializer:
         return supply
 
     #For drawing the bonding curve. Range shows how many times the initial supply you make the graph for, steps how many subdivisions
-    def curve_over_supply(self, range_begin=0, range_end=1000, steps=1000):
+    def curve_over_supply(self, range_begin=0, range_end=1000, steps=500):
         x = np.linspace(range_begin, range_end, steps)
         y = self.get_price(x)
 
-        return pd.DataFrame(zip(x, y), columns=["Supply (in thousands)", "price"])
+        return pd.DataFrame(zip(x, y), columns=["supplyInThousands", "price"])
     
-    def curve_over_balance(self, range_begin=0, range_end=1000, steps=1000):
+    def curve_over_balance(self, range_begin=0, range_end=1000, steps=500):
         supply_list = np.linspace(range_begin, range_end, steps)
         x = self.get_balance(supply_list)
         y = self.get_price(supply_list)
 
-        return pd.DataFrame(zip(x, y), columns=["Balance (in thousands)", "price"])
+        return pd.DataFrame(zip(x, y), columns=["balanceInThousands", "price"])
 
 
 
@@ -76,13 +76,13 @@ class BondingCurve(BondingCurveInitializer):
         self.current_supply = new_supply
         self.current_balance = self.get_balance(new_supply)
 
-    #Returns how much wxDai you get from selling TEC. Informative, doesn't change state
+    #Returns how much wxDAI you get from selling TEC. Informative, doesn't change state
     def sale_return(self, bonded):
         return self.current_balance * (
             (bonded / self.current_supply + 1) ** (1 / self.reserve_ratio()) - 1
         )
 
-    #Returns how much TEC you get from purchasing with wxDai. Informative, doesn't change state
+    #Returns how much TEC you get from purchasing with wxDAI. Informative, doesn't change state
     def purchase_return(self, collateral):
         return self.current_supply * (
             (collateral / self.current_balance + 1) ** (self.reserve_ratio()) - 1
@@ -150,12 +150,12 @@ class BondingCurveHandler():
 
         figure_bonding_curve = self.get_data_augmented_bonding_curve(bondingCurve= self.bonding_curve, min_range=min_range, max_range=max_range, plot_mode=self.plot_mode).to_dict(orient='list')
         
-        reserve_ratio = {"reserve ratio": self.bonding_curve.reserve_ratio()}
+        reserve_ratio = {"reserveRatio": self.bonding_curve.reserve_ratio()}
 
         if self.steps_table.empty:
             return figure_bonding_curve, reserve_ratio
         else: 
-            figure_buy_sell_table = self.steps_table.loc[:,["step", "currentPrice", "amountIn", "tributeCollected", "amountOut", "newPrice", "slippage"]].to_dict(orient='list')
+            figure_buy_sell_table = self.steps_table.loc[:,["step", "currentPriceParsed", "amountIn", "tributeCollected", "amountOut", "newPriceParsed", "slippage"]].to_dict(orient='list')
 
             return figure_bonding_curve, reserve_ratio, figure_buy_sell_table
 
@@ -175,12 +175,14 @@ class BondingCurveHandler():
         column_names = [
             "step",
             "currentPrice",
+            "currentPriceParsed",
             "currentSupply",
             "currentBalance",
             "amountIn",
             "tributeCollected",
             "amountOut",
             "newPrice",
+            "newPriceParsed",
             "newSupply",
             "newBalance",
             "slippage",
@@ -190,23 +192,29 @@ class BondingCurveHandler():
         for index, step in enumerate(steplist):
 
             current_price = bondingCurve.get_price(bondingCurve.current_supply)
+            current_price_parsed = str(format(current_price, '.2f')) + " wxDAI"
+            #current_price_parsed = str(round(current_price, 2)) + " wxDAI"
 
             amount_in = step[0]
-            token_type = step[1]
+            token_type = "wxDAI" if step[1] == "wxDai" else step[1] #to avoid the most obvious error. TO DO: in-depth validation of the steplist...
+            
             amount_in_parsed = str(format(amount_in, '.2f')) + "k " + str(token_type)
+            #amount_in_parsed = str(round(amount_in, 2)) + "k " + str(token_type)
         
             amount_out = 0
             amount_out_parsed = ""
             new_supply = 0
             tribute_collected = 0
-            if token_type == "wxDai":
+            if token_type == "wxDAI":
                 # take tribute and buy
                 tribute_collected = amount_in * bondingCurve.entry_tribute
                 amountAfterTribute = amount_in - tribute_collected
 
                 amount_out = bondingCurve.purchase_return(amountAfterTribute)
                 amount_out_parsed = str(format(amount_out, '.2f')) + "k TEC"
-                tribute_collected_parsed = str(format(tribute_collected, '.2f')) + "k wxDai"
+                #amount_out_parsed = str(round(amount_out, 2)) + "k TEC"
+                tribute_collected_parsed = str(format(tribute_collected, '.2f')) + "k wxDAI"
+                #tribute_collected_parsed = str(round(tribute_collected, 2)) + "k wxDAI"
 
                 slippage = (amount_in - tribute_collected)/bondingCurve.get_price(bondingCurve.current_supply) - amount_out
                 slippage_pct = slippage / ((amount_in - tribute_collected)/bondingCurve.get_price(bondingCurve.current_supply))
@@ -222,9 +230,11 @@ class BondingCurveHandler():
                 amountBeforeTribute = bondingCurve.sale_return(amount_in)            
 
                 tribute_collected = amountBeforeTribute * bondingCurve.exit_tribute #since it is a sale, the number returned is negative
-                tribute_collected_parsed = str(format((tribute_collected*-1), '.2f')) + "k wxDai"
+                tribute_collected_parsed = str(format((tribute_collected*-1), '.2f')) + "k wxDAI"
+                #tribute_collected_parsed = str(round((tribute_collected*-1), 2)) + "k wxDAI"
                 amount_out = (amountBeforeTribute - tribute_collected) #we leave it negative for the supply calculations down below
-                amount_out_parsed = str(format((amount_out*-1), '.2f')) + "k wxDai" 
+                amount_out_parsed = str(format((amount_out*-1), '.2f')) + "k wxDAI" 
+                #amount_out_parsed = str(round((amount_out*-1), 2)) + "k wxDAI"
 
                 slippage = ((amount_in*(1-bondingCurve.exit_tribute))*bondingCurve.get_price(bondingCurve.current_supply) - amount_out) *-1
                 slippage_pct = slippage / ((amount_in*(1-bondingCurve.exit_tribute))*bondingCurve.get_price(bondingCurve.current_supply)) *-1
@@ -235,18 +245,22 @@ class BondingCurveHandler():
                 )
 
             new_price = bondingCurve.get_price(new_supply)
+            new_price_parsed = str(format(new_price, '.2f')) + " wxDAI"
+            #new_price_parsed = str(roud(new_price, 2)) + " wxDAI"
             new_balance = bondingCurve.get_balance(new_supply)
 
             # add to Dataframe
             outputTable.loc[len(outputTable.index)] = [
                 (index+1),
                 round(current_price, 2),
+                current_price_parsed,
                 round(bondingCurve.current_supply, 2),
                 round(bondingCurve.current_balance, 2),
                 amount_in_parsed,
                 tribute_collected_parsed,
                 amount_out_parsed,
                 round(new_price, 2),
+                new_price_parsed,
                 round(new_supply, 2),
                 round(new_balance, 2),
                 slippage_pct,
@@ -272,7 +286,7 @@ class BondingCurveHandler():
 
         if steps_table.empty :
             min_range = 0
-            max_range = 1000
+            max_range = 500
         else:
             min_range = 0 if  zoom_graph == 0 else ( min(steps_table['currentSupply'].min(), steps_table['newSupply'].min()) - 50)
             max_range = steps_table['newSupply'].max() + (200 if zoom_graph == 0 else 50)
@@ -294,6 +308,7 @@ class BondingCurveHandler():
         if scenario_reserve_balance <= 0:
             raise ValueError("Error: Invalid  Hatch Scenario Funding Parameter.")
         if not isinstance(steplist, list):
+            #TO DO: in-depth validation of the steplist
             raise ValueError("Error: Invalid Steplist Parameter.")
         if not (zoom_graph == 0 or zoom_graph == 1):
             raise ValueError("Error: Invalid Graph Zoom Parameter.")
