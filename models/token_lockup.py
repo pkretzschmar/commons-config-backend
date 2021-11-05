847191787cc429567f6971f3736667889e6d240f
Vitor Marthendal <marthendalnunes@gmail.com>
2021-11-04 21:11:20 -0300

    add token lockup token relase chart data

--- models/token_lockup.py
+++ models/token_lockup.py
@@ -38,6 +38,7 @@ class TokenLockupModel:
         df.loc[df['week'] <= self.token_freeze_period, 'price'] = self.opening_price
         df.loc[df['week'] > self.token_freeze_period, 'price'] = (self.opening_price - (df['week'] - self.token_freeze_period) * weekly_token_thaw)
         df.loc[df['price'] < 0, 'price'] = 0
+        df['tokensReleased'] = 100 * (1 - (df['price'] / self.opening_price))
 
         self.output_dict['output'] = {'chart' : df.to_dict(orient='list')}
 
